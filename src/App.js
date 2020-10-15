c16b97746b5c37648c988da59994dd262ec81913
ilyakolbafk <ivkolb@edu.hse.ru>
2020-10-15 17:12:18 +0300

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,78 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import './styles.css'
-function Element(task){
+
+function App() {
     return (
-        <div key={task.id} className="task">
-            <p>Name: {task.name}</p>
-            <p>Description: {task.description}</p>
-            <p>Completed: {task.completed.toString()}</p>
-            <button className="big-button" onClick={() => {
-                console.log("Task "+task.id+" completed status = "+task.completed)
-            }}>Complete this task</button>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
         </div>
-    )
-}
-class MyTodoList extends React.Component {
-    state = {
-        tasks: [
-            {
-                id: 1,
-                name: 'Breakfast',
-                description: 'Сook pancakes',
-                completed: true
-            },
-            {
-                id: 2,
-                name: 'Planning meeting',
-                description: 'Prepare material for the planning meeting',
-                completed: false
-            },
-            {
-                id: 3,
-                name: 'English',
-                description: 'Go to English classes',
-                completed: true
-            },
-            {
-                id: 4,
-                name: 'Rabbit',
-                description: 'Buy dry food for rabbit',
-                completed: true
-            },
-            {
-                id: 5,
-                name: 'Cinema',
-                description: 'Finally go to Tenet',
-                completed: false
-            },
-            {
-                id: 6,
-                name: 'Article',
-                description: 'Write an article about the holiday in English',
-                completed: true
-            },
-            {
-                id: 7,
-                name: 'Car service',
-                description: 'Go to the car service',
-                completed: false
-            },
-            {
-                id: 8,
-                name: 'Relax',
-                description: 'Swing in a hammock',
-                completed: false
-            }
-        ]
-    }
-    render() {
-        return (
-            <main>
-                <h1>To do list</h1>
-                {this.state.tasks.map(task => new Element(task))}
-            </main>
-        )}
+    );
 }
 
-export default MyTodoList;
+export default App;
